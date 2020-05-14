ba941f4dd5533abce28ee41ff65d1d092db1570d
Reinhold Kolm <Reinhold.kolm@gmx.at>
2020-05-14 20:37:55 +0200

    more meetings

--- src/main/java/at/nsdb/nv/Parameter.java
+++ src/main/java/at/nsdb/nv/Parameter.java
@@ -65,7 +65,7 @@ public abstract class Parameter {
 	 * is there a connection due to distance? randomly calculated
 	 */
 	public static boolean calculateRandomlyIfConnection( int distance) {
-		return Utils.randomGetDouble() < Math.min( 0.5, Math.max( 0.01, 0.25 / distance));
+		return Utils.randomGetDouble() < Math.min( 0.5, Math.max( 0.03, 1 / distance ));
 	}
 	
 	
